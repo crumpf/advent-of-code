eab9f4d36fbadef7fa9fbd34abd28a91371b3387
Chris Rumpf <chris.rumpf@genesys.com>
2019-12-19 13:55:36 -0500

    a little day 7, part 1 refactoring

--- 2019/AoC2019 Day 7/AoC2019 Day 7/main.swift
+++ 2019/AoC2019 Day 7/AoC2019 Day 7/main.swift
@@ -19,29 +19,32 @@ let fileInput = try String(contentsOf: fileURL, encoding: .utf8)
 let input = fileInput.components(separatedBy: "\n")[0].components(separatedBy: ",").compactMap { Int($0) }
 let amps = ["A","B","C","D","E"].map { Amplifier(name: "Amp\($0)", program: input) }
 
-var outputSignal: Int?
-let phaseRange = 0...4
-var maxOutput = 0
-var stringDescribingMaxOutput = ""
-
-for num in 0...44444 {
-    outputSignal = nil
-    let phaseSequence = try? String(format: "%05d", num).map { (c) -> Int in
-        guard let num = c.wholeNumberValue, phaseRange.contains(num) else {
-            throw IntcodeError.notFound
+func findMaxOutput(amps: [Amplifier], range: ClosedRange<Int>) -> (sequence: [Int], output: Int) {
+    var outputSignal: Int?
+    var maxOutput = 0
+    var result: (sequence: [Int], output: Int) = ([], 0)
+    for num in 0...44444 {
+        outputSignal = nil
+        let phaseSequence = try? String(format: "%05d", num).map { (c) -> Int in
+            guard let num = c.wholeNumberValue, phaseRange.contains(num) else {
+                throw IntcodeError.notFound
+            }
+            return num
         }
-        return num
+        if let phaseSequence = phaseSequence, phaseSequence.count == Set<Int>(phaseSequence).count, phaseSequence.count == amps.count {
+            for i in 0..<amps.count {
+                outputSignal = amps[i].run(phaseSetting: phaseSequence[i], inputSignal: outputSignal)
+            }
+            if let outputSignal = outputSignal, outputSignal > maxOutput {
+                maxOutput = outputSignal
+                result = (phaseSequence, outputSignal)
+            }
+        }
     }
-    if let phaseSequence = phaseSequence, phaseSequence.count == Set<Int>(phaseSequence).count, phaseSequence.count == amps.count {
-        for i in 0..<amps.count {
-            outputSignal = amps[i].run(phaseSetting: phaseSequence[i], inputSignal: outputSignal)
-        }
-        if let outputSignal = outputSignal, outputSignal > maxOutput {
-            maxOutput = outputSignal
-            stringDescribingMaxOutput = "\(phaseSequence) : \(String(describing: outputSignal))"
-        }
-    }
+    return result
 }
 
-print(stringDescribingMaxOutput)
+let phaseRange = 0...4
+let part1 = findMaxOutput(amps: amps, range: phaseRange)
 
+print("Part1 Answer: \(part1.sequence) : \(part1.output)")
