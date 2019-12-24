ab8c5a6f2281899bc8f6e86be6e2d796ba67409c
Chris Rumpf <chris.rumpf@genesys.com>
2019-12-24 01:12:15 -0500

    day 8, part 1, passed

--- 2019/AoC2019 Day 8/AoC2019 Day 8/main.swift
+++ 2019/AoC2019 Day 8/AoC2019 Day 8/main.swift
@@ -17,3 +17,81 @@ let fileURL = URL(fileURLWithPath: "input.txt", relativeTo: URL(fileURLWithPath:
 let fileInput = try String(contentsOf: fileURL, encoding: .utf8)
 
 let input = fileInput.components(separatedBy: "\n")[0]
+let imageData = Array(input).compactMap { Int(String($0)) }
+
+
+func printLayer(_ layer: [[Int]]) {
+    for array in layer {
+        for value in array {
+            print(value, terminator: " ")
+        }
+        print(" ")
+    }
+}
+
+class ElfBitmap {
+    let width: Int
+    let height: Int
+    var layers: [[[Int]]]
+
+    init?(width: Int, height: Int, input: [Int]) {
+        guard input.count % (width * height) == 0 else {
+            return nil
+        }
+
+        self.width = width
+        self.height = height
+        self.layers = [[[Int]]]()
+
+        var i = 0
+        var layer = Array(repeating: Array(repeating: 0, count: width), count: height)
+        while i < input.count {
+            layer[i/width % height][i % width] = input[i]
+            i += 1
+            if i % (width * height) == 0 {
+                layers.append(layer)
+                layer = Array(repeating: Array(repeating: 0, count: width), count: height)
+            }
+        }
+    }
+}
+
+if let part1 = ElfBitmap(width: 25, height: 6, input: imageData) {
+    // find layer with fewest 0 digits
+    var zeroCount: Int
+    var minZerosFound = Int.max
+    var minZerosLayer = -1
+    var layerIndex = 0
+    for lyr in part1.layers {
+        zeroCount = 0
+        for row in lyr {
+            for val in row {
+                if val == 0 {
+                    zeroCount += 1
+                }
+            }
+        }
+        if zeroCount < minZerosFound {
+            minZerosFound = zeroCount
+            minZerosLayer = layerIndex
+        }
+        layerIndex += 1
+    }
+
+    print("min zeros: layer \(minZerosLayer), numZeroes \(minZerosFound)")
+
+    let minLayer = part1.layers[minZerosLayer]
+    var onesCount = 0
+    var twosCount = 0
+    for row in minLayer {
+        for val in row {
+            if val == 1 {
+                onesCount += 1
+            } else if val == 2 {
+                twosCount += 1
+            }
+        }
+    }
+
+    print("the number of 1 digits multiplied by the number of 2 digits = \(onesCount * twosCount)")
+}
