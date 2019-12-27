9c2794faf9ed25ba92f3a3c32d84cdbecad70070
Chris Rumpf <chris.rumpf@genesys.com>
2019-12-27 15:18:01 -0500

    scaffold for day 11

---
+++ 2019/AoC2019 Day 11/AoC2019 Day 11/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  AoC2019 Day 11
+//
+//  Created by Chris Rumpf on 12/27/19.
+//  Copyright © 2019 Chris Rumpf. All rights reserved.
+//
+
+import Foundation
+
+let fileURL = URL(fileURLWithPath: "input.txt", relativeTo: URL(fileURLWithPath: FileManager.default.currentDirectoryPath))
+let fileInput = try String(contentsOf: fileURL, encoding: .utf8)
+
+let input = fileInput.components(separatedBy: "\n")[0].components(separatedBy: ",").compactMap { Int($0) }
+
+print(input)
