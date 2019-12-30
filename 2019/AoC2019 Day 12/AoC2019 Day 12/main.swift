764d20ef7c4ee589baa291f45b5bb08893643663
Chris Rumpf <chris.rumpf@genesys.com>
2019-12-30 16:23:04 -0500

    day 12, part 2, passed

--- 2019/AoC2019 Day 12/AoC2019 Day 12/main.swift
+++ 2019/AoC2019 Day 12/AoC2019 Day 12/main.swift
@@ -14,7 +14,7 @@ let fileInput = try String(contentsOf: fileURL, encoding: .utf8)
 // Day 12: The N-Body Problem
 
 //Each moon has a 3-dimensional position (x, y, and z) and a 3-dimensional velocity
-struct Moon {
+struct Moon: Hashable {
     var position: SIMD3<Int>
     var velocity: SIMD3<Int>
 
@@ -28,6 +28,18 @@ struct Moon {
 
     func kin() -> Int {
         abs(velocity.x) + abs(velocity.y) + abs(velocity.z)
+    }
+
+    func posVelX() -> SIMD2<Int> {
+        SIMD2(position.x, velocity.x)
+    }
+
+    func posVelY() -> SIMD2<Int> {
+        SIMD2(position.y, velocity.y)
+    }
+
+    func posVelZ() -> SIMD2<Int> {
+        SIMD2(position.z, velocity.z)
     }
 }
 
@@ -83,18 +95,89 @@ func step(moons: [Moon]) -> [Moon] {
     return updated.map { Moon(position: $0.position &+ $0.velocity, velocity: $0.velocity) }
 }
 
-//let testData = "<x=-1, y=0, z=2>\n<x=2, y=-10, z=-7>\n<x=4, y=-8, z=8>\n<x=3, y=5, z=-1>\n".components(separatedBy: "\n")
-//let moons = parse(testData)
-let moons = parse(fileInput.components(separatedBy: "\n"))
-var steps: [[Moon]] = [moons]
+// PART 1
+
+let testData = "<x=-1, y=0, z=2>\n<x=2, y=-10, z=-7>\n<x=4, y=-8, z=8>\n<x=3, y=5, z=-1>\n".components(separatedBy: "\n")
+//var moons = parse(testData)
+let originalInput = parse(fileInput.components(separatedBy: "\n"))
+var moons = originalInput
 
 for _ in 1...1000 {
-    steps.append(step(moons: steps.last!))
-    print(steps.last!)
+    moons = step(moons: moons)
+    print(moons)
 }
 
-let totalEnergy = steps.last?.reduce(0, { (result, moon) -> Int in
+let totalEnergy = moons.reduce(0, { (result, moon) -> Int in
     result + moon.pot() * moon.kin()
 })
 
 print("total energy: \(String(describing: totalEnergy))")
+
+
+// PART 2
+
+
+// hint: the x, y and z axes are independent and repeat
+
+// can I find the period that each of the axes repeats? If so, should be able to find the
+// least common multiple of each rather than brute forcing 
+
+var x=0, y=0, z=0
+
+moons = originalInput
+var hashes = Set([moons.map { $0.posVelX() }.hashValue])
+var step = 0
+while true {
+    step += 1
+    moons = step(moons: moons)
+    if hashes.contains(moons.map { $0.posVelX() }.hashValue) {
+        x = step
+        break
+    } else {
+        hashes.insert(moons.map { $0.posVelX() }.hashValue)
+    }
+}
+
+moons = originalInput
+hashes = Set([moons.map { $0.posVelY() }.hashValue])
+step = 0
+while true {
+    step += 1
+    moons = step(moons: moons)
+    if hashes.contains(moons.map { $0.posVelY() }.hashValue) {
+        y = step
+        break
+    } else {
+        hashes.insert(moons.map { $0.posVelY() }.hashValue)
+    }
+}
+
+moons = originalInput
+hashes = Set([moons.map { $0.posVelZ() }.hashValue])
+step = 0
+while true {
+    step += 1
+    moons = step(moons: moons)
+    if hashes.contains(moons.map { $0.posVelZ() }.hashValue) {
+        z = step
+        break
+    } else {
+        hashes.insert(moons.map { $0.posVelZ() }.hashValue)
+    }
+}
+
+func gcd(_ a: Int, _ b: Int) -> Int {
+    // Euclidean algorithm
+    if b == 0 {
+        return a
+    }
+    return gcd(b, a % b)
+}
+
+func lcm(_ a: Int, _ b: Int) -> Int {
+    a * b / gcd(a, b)
+}
+
+print("took \(x) \(y) \(z) steps to reach the first state that exactly matches a previous state.")
+
+print("lcm is \(lcm(lcm(x, y), z))")
