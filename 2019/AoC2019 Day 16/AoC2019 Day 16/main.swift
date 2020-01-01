e2f84e8d93c5847d419e37ef842e08aeec06bb8f
Chris Rumpf <chris.rumpf@genesys.com>
2020-01-01 18:12:45 -0500

    empty projects for days 15 and 16

---
+++ 2019/AoC2019 Day 16/AoC2019 Day 16/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AoC2019 Day 16
+//
+//  Created by Chris Rumpf on 1/1/20.
+//  Copyright © 2020 Chris Rumpf. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
