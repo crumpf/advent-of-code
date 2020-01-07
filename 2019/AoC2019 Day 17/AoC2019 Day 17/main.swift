e0dec264efb0c8b63abbccc49f9e1202b76d13b2
Chris Rumpf <chris.rumpf@genesys.com>
2020-01-07 13:38:53 -0500

    day 17, part 2, passed

--- 2019/AoC2019 Day 17/AoC2019 Day 17/main.swift
+++ 2019/AoC2019 Day 17/AoC2019 Day 17/main.swift
@@ -19,7 +19,7 @@ func characterFromScalar(_ scalar: Int) -> Character {
 
 typealias Location = SIMD2<Int>
 
-func part1() {
+func part1() -> [[String]] {
     let program = fileInput.components(separatedBy: "\n")[0].components(separatedBy: ",").compactMap { Int($0) }
     let computer = IntcodeComputer(program: program)
 
@@ -63,5 +63,96 @@ func part1() {
     print(intersections.reduce(0) { (res, loc) -> Int in
         res + loc.x * loc.y
     })
+
+    return map
 }
-part1()
+let p1 = part1()
+
+print("\nNow for PART 2\n")
+
+func part2() -> ([[String]], Int?) {
+    var program = fileInput.components(separatedBy: "\n")[0].components(separatedBy: ",").compactMap { Int($0) }
+    // Force the vacuum robot to wake up by changing the value in your ASCII program at address 0 from 1 to 2
+    program[0] = 2
+    let computer = IntcodeComputer(program: program)
+
+    // build map
+
+    var map: [[String]] = []
+    var currentRow: [String] = []
+    while computer.state != .waitingForInput {
+        if let output = computer.run(input: []) {
+            if output == 10 {
+                map.append(currentRow)
+                currentRow = []
+            } else if let scalar = UnicodeScalar(output) {
+                let s = String(Character(scalar))
+                currentRow.append(s)
+            }
+        }
+    }
+    for row in map {
+        print(row.reduce("", { (result, s) -> String in
+            result + s
+        }))
+    }
+
+    let fnMain = "A,B,A,C,B,C,B,C,A,B"
+    let fnA = "L,6,L,4,R,8"
+    let fnB = "R,8,L,6,L,4,L,10,R,8"
+    let fnC = "L,4,R,4,L,4,R,8"
+
+    var main = fnMain.components(separatedBy: ",").flatMap { $0.map { Int($0.asciiValue!) } + [44] }
+    main[main.count - 1] = 10
+    var a = fnA.components(separatedBy: ",").flatMap { $0.map { Int($0.asciiValue!) } + [44] }
+    a[a.count - 1] = 10
+    var b = fnB.components(separatedBy: ",").flatMap { $0.map { Int($0.asciiValue!) } + [44] }
+    b[b.count - 1] = 10
+    var c = fnC.components(separatedBy: ",").flatMap { $0.map { Int($0.asciiValue!) } + [44] }
+    c[c.count - 1] = 10
+
+    var prompt: [Character] = []
+    var output: Int?
+    var result = 0
+
+    for routine in [main, a, b, c] {
+        while computer.state != .waitingForInput {
+            if let output = computer.run(input: []) {
+                prompt.append(Character(UnicodeScalar(output)!))
+            }
+        }
+        print(String(prompt))
+        prompt.removeAll()
+        if let output = computer.run(input: routine) {
+            prompt.append(Character(UnicodeScalar(output)!))
+        }
+    }
+
+    while computer.state != .waitingForInput {
+        if let output = computer.run(input: []) {
+            prompt.append(Character(UnicodeScalar(output)!))
+        }
+    }
+    print(String(prompt))
+    prompt.removeAll()
+    output = computer.run(input: [Int(Character("n").asciiValue!), 10])
+
+    while computer.state != .halted {
+        if let output = computer.run(input: []) {
+            if let scalar = UnicodeScalar(output) {
+                if scalar.isASCII {
+                    prompt.append(Character(scalar))
+                } else {
+                    result = output
+                }
+            }
+        }
+    }
+
+    print(String(prompt))
+
+    return (map, result)
+}
+let p2 = part2()
+
+print("dust collected = \(String(describing: p2.1))")
