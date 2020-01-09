134703cf0d9ce5c5cbd4a38ce96979e1df93c3e1
Chris Rumpf <chris.rumpf@genesys.com>
2020-01-09 16:57:33 -0500

    day 19, part 2, passed

--- 2019/AoC2019 Day 19/AoC2019 Day 19/main.swift
+++ 2019/AoC2019 Day 19/AoC2019 Day 19/main.swift
@@ -18,31 +18,46 @@ let fileInput = try String(contentsOf: fileURL, encoding: .utf8)
 
 typealias Location = SIMD2<Int>
 
+class BeamComputer {
+    let computer: IntcodeComputer
+
+    init(program: [Int]) {
+        computer = IntcodeComputer(program: program)
+    }
+
+    func isBeamAt(x: Int, y: Int) -> Bool {
+        computer.reset()
+        while computer.state != .waitingForInput {
+            // get ready for input
+            _ = computer.run(input: [])
+        }
+        if let output = computer.run(input: [x, y]) {
+            return output == 1
+        }
+        return false
+    }
+}
+
 // How many points are affected by the tractor beam in the 50x50 area closest to the emitter
 func part1() -> Int? {
     let program = fileInput.components(separatedBy: "\n")[0].components(separatedBy: ",").compactMap { Int($0) }
-    let computer = IntcodeComputer(program: program)
+    let computer = BeamComputer(program: program)
+
 
     var map: [[String]] = Array(repeating: Array(repeating: ".", count: 50), count: 50)
     var beam = Set<Location>()
-    var output: Int?
 
     for row in 0..<50 {
         for col in 0..<50 {
-            computer.reset()
-            while computer.state != .waitingForInput {
-                output = computer.run(input: [])
-            }
-            output = computer.run(input: [col, row])
-            if let output = output, output == 1 {
+            if computer.isBeamAt(x: col, y: row) {
                 beam.insert(Location(col, row))
                 map[row][col] = "#"
             }
         }
     }
 
-    for row in map {
-        print(row.joined())
+    for (i, row) in map.enumerated() {
+        print("\(i)\(row.joined())")
     }
 
     return beam.count
@@ -50,3 +65,36 @@ func part1() -> Int? {
 
 print("PART 1")
 print("points affected: \(part1())")
+
+// Find the 100x100 square closest to the emitter that fits entirely within the tractor beam; within that square, find the point closest to the emitter. What value do you get if you take that point's X coordinate, multiply it by 10000, then add the point's Y coordinate?
+func part2(width: Int, height: Int) -> Location {
+    let program = fileInput.components(separatedBy: "\n")[0].components(separatedBy: ",").compactMap { Int($0) }
+    let computer = BeamComputer(program: program)
+
+    var row = max(5, height - 1) // start at first possible maxY value, but rows in 1...4 don't appear to have any beam locations
+    var col = 0
+    while true {
+        var minColFound = false
+        while !minColFound {
+            if computer.isBeamAt(x: col, y: row) {
+                minColFound = true
+            } else {
+                col += 1
+            }
+        }
+        // (minX, maxY) found, now check for (maxX, minY) in beam
+        if computer.isBeamAt(x: col + (width - 1), y: row - (height - 1)) {
+            break
+        } else {
+            row += 1
+        }
+    }
+
+    // square closest to the emitter
+    return Location(col, row - (height - 1))
+}
+
+print("PART 2")
+let point = part2(width: 100, height: 100)
+print("point: \(point)")
+print("answer: \(point.x * 10000 + point.y)")
