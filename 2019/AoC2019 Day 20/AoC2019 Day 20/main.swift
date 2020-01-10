54a713bb4ecf625141b93f3e0e12843424eabe6a
Chris Rumpf <chris.rumpf@genesys.com>
2020-01-10 10:21:06 -0500

    day 20 project ready

---
+++ 2019/AoC2019 Day 20/AoC2019 Day 20/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  AoC2019 Day 20
+//
+//  Created by Chris Rumpf on 1/10/20.
+//  Copyright © 2020 Chris Rumpf. All rights reserved.
+//
+
+import Foundation
+
+let fileURL = URL(fileURLWithPath: "input.txt", relativeTo: URL(fileURLWithPath: FileManager.default.currentDirectoryPath))
+let fileInput = try String(contentsOf: fileURL, encoding: .utf8)
+
+// --- Day 20: Donut Maze ---
+
+
