2d0330a29908c6a99d6e4c1a5e734cbd4d83bf03
Christopher Rumpf <crumpf@bluebrindle.org>
2020-12-06 01:00:19 -0500

    AoC 2020 Day 6

--- 2020/Swift/Advent of Code/Day 6/main.swift
+++ 2020/Swift/Advent of Code/Day 6/main.swift
@@ -7,23 +7,68 @@
 
 import Foundation
 
-class Day {
+class Day6 {
   
 }
 
-extension Day: Puzzle {
+extension Day6: Puzzle {
   func part1(withInput input: String) -> String {
-    return "Not implemented"
+    String(
+      input.trimmingCharacters(in: .whitespacesAndNewlines)
+        .components(separatedBy: "\n\n")
+        .map { Set($0).subtracting("\n") }
+        .reduce(0) { (result, answers) -> Int in
+          result + answers.count
+        }
+    )
   }
   
   func part2(withInput input: String) -> String {
-    return "Not implemented"
+    
+    let answer = input.trimmingCharacters(in: .whitespacesAndNewlines)
+      .components(separatedBy: "\n\n")
+      .map { $0.split(separator: "\n").map { Set($0)} }
+      .map { answers -> Int in
+        let common = answers.reduce(answers.first!) { (result, set) in
+          result.intersection(set)
+        }
+        return common.count
+      }
+      .reduce(0) { $0 + $1 }
+    
+    return String(answer)
   }
 }
 
+let testInput = """
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+"""
+
 guard let fileInput = FileInput(pathRelativeToCurrentDirectory: "input.txt") else { abort() }
 
-let day = Day()
+let day = Day6()
+
+print("====Test 1====")
+let test1 = day.part1(withInput: testInput)
+print(test1)
+
+print("====Test 2====")
+let test2 = day.part2(withInput: testInput)
+print(test2)
 
 print("====Part 1====")
 let part1 = day.part1(withInput: fileInput.raw)
