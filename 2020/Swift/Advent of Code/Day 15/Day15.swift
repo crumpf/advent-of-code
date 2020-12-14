86920da99f403ddca579300e05d1060c003a91c4
Christopher Rumpf <crumpf@bluebrindle.org>
2020-12-14 13:20:44 -0500

    setting up day 15

---
+++ 2020/Swift/Advent of Code/Day 15/Day15.swift
@@ -0,0 +1,19 @@
+//
+//  Day15.swift
+//  Advent of Code
+//
+//  Created by Christopher Rumpf on 12/11/20.
+//
+
+import Foundation
+
+class Day15: Day {
+  func part1() -> String {
+    "Not Implemented"
+  }
+  
+  func part2() -> String {
+    "Not Implemented"
+  }
+}
+
