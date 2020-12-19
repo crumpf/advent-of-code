db6290316e9c5adad626f2408fdaa95b3135dfa7
Christopher Rumpf <crumpf@bluebrindle.org>
2020-12-19 00:02:25 -0500

    stubbing in Day 19

---
+++ 2020/Swift/Advent of Code/Day 19/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  Day 19 
+//
+//  Created by Christopher Rumpf on 12/12/20.
+//
+
+import Foundation
+
+guard let fileInput = FileInput(pathRelativeToCurrentDirectory: "input.txt") else { abort() }
+var startTime = 0.0
+let day = Day19(input: fileInput.raw)
+
+print("====Part 1====")
+startTime = CFAbsoluteTimeGetCurrent()
+let part1 = day.part1()
+print("result: \(part1), duration: \((CFAbsoluteTimeGetCurrent()-startTime)*1000)ms")
+
+print("====Part 2====")
+startTime = CFAbsoluteTimeGetCurrent()
+let part2 = day.part2()
+print("result: \(part2), duration: \((CFAbsoluteTimeGetCurrent()-startTime)*1000)ms")
+
