bb3580df767875d128ef4ffef7420b7744ba596c
Christopher Rumpf <crumpf@bluebrindle.org>
2020-12-20 23:51:13 -0500

    stubbing in day 21

---
+++ 2020/Swift/Advent of Code/Day 21/Day21.swift
@@ -0,0 +1,19 @@
+//
+//  Day21.swift
+//  Advent of Code
+//
+//  Created by Christopher Rumpf on 12/11/20.
+//
+
+import Foundation
+
+class Day21: Day {
+  func part1() -> String {
+    "Not Implemented"
+  }
+  
+  func part2() -> String {
+    "Not Implemented"
+  }
+}
+
