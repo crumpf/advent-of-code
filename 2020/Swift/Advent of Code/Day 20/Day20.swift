c9e22e65cba827f0a48d8e7d70c73f55525fbb32
Christopher Rumpf <crumpf@bluebrindle.org>
2020-12-25 23:14:00 -0500

    sped up pt 1 day 20 by almost 10x

--- 2020/Swift/Advent of Code/Day 20/Day20.swift
+++ 2020/Swift/Advent of Code/Day 20/Day20.swift
@@ -18,6 +18,30 @@ enum Orientation: CaseIterable {
   case flipped270
 }
 
+struct Border {
+  let top, left, bottom, right: String
+  func makeBorder(orientation: Orientation) -> Border {
+    switch orientation {
+    case .initial0:
+      return self
+    case .initial90:
+      return Border(top: right, left: String(top.reversed()), bottom: left, right: String(bottom.reversed()))
+    case .initial180:
+      return Border(top: String(bottom.reversed()), left: String(right.reversed()), bottom: String(top.reversed()), right: String(left.reversed()))
+    case .initial270:
+      return Border(top: String(left.reversed()), left: bottom, bottom: String(right.reversed()), right: top)
+    case .flipped0:
+      return Border(top: bottom, left: String(left.reversed()), bottom: top, right: String(right.reversed()))
+    case .flipped90:
+      return Border(top: String(right.reversed()), left: String(bottom.reversed()), bottom: String(left.reversed()), right: String(top.reversed()))
+    case .flipped180:
+      return Border(top: String(top.reversed()), left: right, bottom: String(bottom.reversed()), right: left)
+    case .flipped270:
+      return Border(top: left, left: top, bottom: right, right: bottom)
+    }
+  }
+}
+
 class ImageTile: Hashable {
   init?(data: String) {
     let components = data.components(separatedBy: ":\n")
@@ -25,82 +49,85 @@ class ImageTile: Hashable {
           let id = Int(components[0].dropFirst("Tile ".count))
     else {
       return nil
-      
     }
+    var lines = components[1].lines()
+    
     self.id = id
-    self.initialBitmap = components[1].lines()
+    let left = String(lines.compactMap { $0.first })
+    let right = String(lines.compactMap { $0.last })
+    let top = lines.removeFirst()
+    let bottom = lines.removeLast()
+    initialBorder = Border(top: top, left: left, bottom: bottom, right: right)
+    initialBitmap = lines.map { String($0[1..<($0.count-1)]) }
     guard initialBitmap.count == initialBitmap[0].count else { return nil } // must be a square image
     orientation = .initial0
-    bitmap = initialBitmap
+    border = initialBorder
   }
   
   func hash(into hasher: inout Hasher) { hasher.combine(id) }
   static func == (lhs: ImageTile, rhs: ImageTile) -> Bool { lhs.id == rhs.id }
   
   let id: Int
+  private let initialBorder: Border
   private let initialBitmap: [String]
-  private(set) var bitmap: [String]
-  var rowCount: Int { bitmap.count }
-  var colCount: Int { bitmap.isEmpty ? 0 : bitmap[0].count }
+  var rowCount: Int { initialBitmap.count }
+  var colCount: Int { initialBitmap.isEmpty ? 0 : initialBitmap[0].count }
   var orientation: Orientation {
     didSet {
-      let maxIdx = rowCount - 1
-      switch orientation {
-      case .initial0:
-        bitmap = initialBitmap
-      case .initial90:
-        bitmap.indices.forEach { i in
-          bitmap[i] = String(initialBitmap.compactMap { $0[maxIdx - i] })
-        }
-      case .initial180:
-        bitmap.indices.forEach { i in
-          bitmap[i] = String(initialBitmap[maxIdx - i].reversed())
-        }
-      case .initial270:
-        bitmap.indices.forEach { i in
-          bitmap[i] = String(initialBitmap.compactMap { $0[i] }.reversed())
-        }
-      case .flipped0:
-        bitmap.indices.forEach { i in
-          bitmap[i] = initialBitmap[maxIdx - i]
-        }
-      case .flipped90:
-        bitmap.indices.forEach { i in
-          bitmap[i] = String(initialBitmap.compactMap { $0[maxIdx - i] }.reversed())
-        }
-      case .flipped180:
-        bitmap.indices.forEach { i in
-          bitmap[i] = String(initialBitmap[i].reversed())
-        }
-      case .flipped270:
-        bitmap.indices.forEach { i in
-          bitmap[i] = String(initialBitmap.compactMap { $0[i] })
-        }
-      }
+      border = initialBorder.makeBorder(orientation: orientation)
     }
   }
-  var topEdge: String { bitmap.first! }
-  var leftEdge: String { String(bitmap.compactMap { $0.first }) }
-  var bottomEdge: String { bitmap.last! }
-  var rightEdge: String { String(bitmap.compactMap { $0.last }) }
+  private(set) var border: Border
   var topTile: ImageTile?
   var leftTile: ImageTile?
   var bottomTile: ImageTile?
   var rightTile: ImageTile?
   
+  func makeBitmap() -> [String] {
+    var bitmap = [String]()
+    let maxIdx = rowCount - 1
+    switch orientation {
+    case .initial0:
+      return initialBitmap
+    case .initial90:
+      bitmap.indices.forEach { i in
+        bitmap[i] = String(initialBitmap.compactMap { $0[maxIdx - i] })
+      }
+    case .initial180:
+      bitmap.indices.forEach { i in
+        bitmap[i] = String(initialBitmap[maxIdx - i].reversed())
+      }
+    case .initial270:
+      bitmap.indices.forEach { i in
+        bitmap[i] = String(initialBitmap.compactMap { $0[i] }.reversed())
+      }
+    case .flipped0:
+      bitmap.indices.forEach { i in
+        bitmap[i] = initialBitmap[maxIdx - i]
+      }
+    case .flipped90:
+      bitmap.indices.forEach { i in
+        bitmap[i] = String(initialBitmap.compactMap { $0[maxIdx - i] }.reversed())
+      }
+    case .flipped180:
+      bitmap.indices.forEach { i in
+        bitmap[i] = String(initialBitmap[i].reversed())
+      }
+    case .flipped270:
+      bitmap.indices.forEach { i in
+        bitmap[i] = String(initialBitmap.compactMap { $0[i] })
+      }
+    }
+    return bitmap
+  }
   
   func printData() {
     print("Tile \(id): \(orientation)")
-    bitmap.forEach { print($0) }
+    makeBitmap().forEach { print($0) }
   }
 }
 
 class Day20: Day {
-  
-  /*
-   A few notes about this pt 1 solution. It takes around 30 seconds with the puzzle input to flip all the pieces around and do all the comparisons in the way I approached it. Painfully slow.
-   I should optimize and calculate the edge values and compare the tiles based on those instead of all repeated orientation changes in the algorithm below checking for matches causing underlying bitmap changes. But, I've been banging away at this puzzle for so long I'm just happy to have found the answer correct answer. Maybe one day I'll tackle this again.
-   */
   
   /// To check that you've assembled the image correctly, multiply the IDs of the four corner tiles together.
   func part1() -> String {
@@ -117,10 +144,10 @@ class Day20: Day {
           if t1 == t2 { continue }
           for o2 in Orientation.allCases {
             t2.orientation = o2
-            if t1.leftEdge == t2.rightEdge { matches += 1 }
-            else if t1.rightEdge == t2.leftEdge { matches += 1 }
-            else if t1.topEdge == t2.bottomEdge { matches += 1 }
-            else if t1.bottomEdge == t2.topEdge { matches += 1 }
+            if t1.border.left == t2.border.right { matches += 1 }
+            else if t1.border.right == t2.border.left { matches += 1 }
+            else if t1.border.top == t2.border.bottom { matches += 1 }
+            else if t1.border.bottom == t2.border.top { matches += 1 }
           }
           if matches > mostMatchesFound { mostMatchesFound = matches }
         }
