2247385d8f1b3624092d24c31c65027a668fdda1
Chris Rumpf <crumpf@bluebrindle.org>
2021-12-04 09:48:25 -0500

    Day 4

---
+++ 2021/Swift/Advent of Code/Day 4/Day4.swift
@@ -0,0 +1,21 @@
+//
+//  Day4.swift
+//  Advent of Code
+//
+//  Created by Christopher Rumpf on 12/4/21.
+//
+//  https://adventofcode.com/2021/day/4
+
+import Foundation
+
+class Day4: Day {
+  ///
+  func part1() -> String {
+    return ""
+  }
+  
+  ///
+  func part2() -> String {
+    return ""
+  }
+}
