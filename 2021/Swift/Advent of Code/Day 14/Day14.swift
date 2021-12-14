d62ddec6c237040c599815beac0b01ccebe35b20
Chris Rumpf <crumpf@bluebrindle.org>
2021-12-13 23:05:26 -0500

    .

--- 2021/Swift/Advent of Code/Day 14/Day14.swift
+++ 2021/Swift/Advent of Code/Day 14/Day14.swift
@@ -9,11 +9,11 @@ import Foundation
 
 class Day14: Day {
   func part1() -> String {
-    "Not Implemented"
+    return ""
   }
   
   func part2() -> String {
-    "Not Implemented"
+    return ""
   }
 }
 
