89794199dfc4d40fb96bac15fb9bc405fb44f31d
Chris Rumpf <crumpf@bluebrindle.org>
2021-12-14 12:42:44 -0500

    Day 14, Part 2

--- 2021/Swift/Advent of Code/Day 14/Day14.swift
+++ 2021/Swift/Advent of Code/Day 14/Day14.swift
@@ -25,18 +25,61 @@ class Day14: Day {
     return "\(elementCounts[ordered.first!]! - elementCounts[ordered.last!]!)"
   }
   
+  // The polymer length grows exponentially and quickly grows too large for my full polymer
+  // growth in part1. So, a new approach is needed. This approach will not allow us to see the
+  // ordered sequence of the polymer, but we can count the elements at each stage this way which
+  // is sufficient to solve today's problem.
   func part2() -> String {
-    return ""
+    let instructions = PolymerInstructions(input: input)
+    /// running total of counts for elements added at each stage, seeded with the instruction template
+    var totalElementCounts: [String: Int] = instructions.polymerTemplate
+      .reduce(into: [String: Int]()) { partialResult, c in
+        let element = String(c)
+        partialResult[element] = 1 + (partialResult[element] ?? 0)
+      }
+    /// the element pairs  and their number of occurences for a given step, seeded with the pairs found in the instruction template
+    var stepPairCounts: [String: Int] = pairs(inTemplate: instructions.polymerTemplate)
+      .reduce(into: [String: Int]()) { partialResult, pair in
+        partialResult[pair] = 1 + (partialResult[pair] ?? 0)
+      }
+    
+    for _ in 1...40 {
+      /// at each step, we will calculate the pairs to be used in the future step and their number of occurences
+      var nextPairs: [String: Int] = [:]
+      for pair in stepPairCounts {
+        if let insert = instructions.pairInsertions[pair.key] {
+          // 1. map the pair to the element to insert and increase it's count in our map of element counts (we're inserting this many more of the element into the polymer)
+          totalElementCounts[insert] = pair.value + (totalElementCounts[insert] ?? 0)
+          // 2. figure out what pairs we'll explore in the next step. add 2 new pairs resulting from inserting the new element into the nextPairs map
+          let first = String(pair.key[0]) + insert
+          let second = insert + String(pair.key[1])
+          nextPairs[first] = pair.value + (nextPairs[first] ?? 0)
+          nextPairs[second] = pair.value + (nextPairs[second] ?? 0)
+        }
+      }
+      stepPairCounts = nextPairs
+    }
+    
+    let ordered = totalElementCounts.sorted { $0.value > $1.value }
+    
+    return "\(ordered.first!.value - ordered.last!.value)"
   }
   
   func polymer(forTemplate template: String, pairInsertions: [String: String]) -> String {
     var polymer = ""
-    for n in 0..<template.count-1 {
-      let pair = String(template[n...n+1])
+    for pair in pairs(inTemplate: template) {
       let element = pairInsertions[pair]
       polymer += "\(pair.first!)\(element!)"
     }
     return "\(polymer)\(template.last!)"
+  }
+  
+  func pairs(inTemplate template: String) -> [String] {
+    var pairs: [String] = []
+    for n in 0..<template.count-1 {
+      pairs.append(String(template[n...n+1]))
+    }
+    return pairs
   }
 }
 
