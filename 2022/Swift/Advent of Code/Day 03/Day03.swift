c15c3108a819798be42069a09daa2ea3d7f279d2
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2022-12-02 12:01:37 -0500

    setup Day 3

---
+++ 2022/Swift/Advent of Code/Day 03/Day03.swift
@@ -0,0 +1,19 @@
+//
+//  Day03.swift
+//  Advent of Code
+//
+//  Created by Christopher Rumpf on 12/02/22.
+//
+
+import Foundation
+
+class Day03: Day {
+  func part1() -> String {
+    "Not Implemented"
+  }
+  
+  func part2() -> String {
+    "Not Implemented"
+  }
+}
+
