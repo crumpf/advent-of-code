dc6d242a6fcfd09d0830e348fb346b849fb63383
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2022-12-03 00:55:09 -0500

    Day 3

--- 2022/Swift/Advent of Code/Day 03/Day03.swift
+++ 2022/Swift/Advent of Code/Day 03/Day03.swift
@@ -8,12 +8,41 @@
 import Foundation
 
 class Day03: Day {
-  func part1() -> String {
-    "Not Implemented"
-  }
-  
-  func part2() -> String {
-    "Not Implemented"
-  }
+    func part1() -> String {
+        let repeatedItemTypes = input
+            .lines()
+            .compactMap {
+                let mid = $0.count / 2
+                let left = Set($0[0..<mid])
+                let right = Set($0[mid..<$0.count])
+                return left.intersection(right).first
+            }
+        let result = sumOfPrioritiesOfItemTypes(repeatedItemTypes)
+        return "\(result)"
+    }
+    
+    func part2() -> String {
+        let lines = input.lines()
+        var badgeItemTypes: [Character] = []
+        for i in stride(from: 0, to: lines.count, by: 3) {
+            let set = Set(lines[i]).intersection(Set(lines[i+1])).intersection(Set(lines[i+2]))
+            badgeItemTypes.append(set.first!)
+        }
+        let result = sumOfPrioritiesOfItemTypes(badgeItemTypes)
+        return "\(result)"
+    }
+    
+    private func sumOfPrioritiesOfItemTypes(_ itemTypes: [Character]) -> Int {
+        itemTypes.reduce(0) { partialResult, item in
+            var priority = 0
+            if item.isLowercase {
+                priority = Int(item.asciiValue! - Character("a").asciiValue!) + 1
+            } else {
+                priority = Int(item.asciiValue! - Character("A").asciiValue!) + 27
+            }
+            return partialResult + priority
+        }
+    }
+    
 }
 
