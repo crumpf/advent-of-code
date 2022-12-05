ea9f0e41fa9c1c985a7cb9dc281d43e840da88a3
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2022-12-05 11:48:39 -0500

    Day 6 setup

---
+++ 2022/Swift/Advent of Code/Day 06/DayX.swift
@@ -0,0 +1,19 @@
+//
+//  Day06.swift
+//  Advent of Code
+//
+//  Created by Christopher Rumpf on 12/06/20.
+//
+
+import Foundation
+
+class Day06: Day {
+  func part1() -> String {
+    "Not Implemented"
+  }
+  
+  func part2() -> String {
+    "Not Implemented"
+  }
+}
+
--- 2022/Swift/Advent of Code/Template/DayX.swift
+++ 2022/Swift/Advent of Code/Template/DayX.swift
@@ -2,18 +2,18 @@
 //  DayX.swift
 //  Advent of Code
 //
-//  Created by Christopher Rumpf on 12/11/20.
+//  Created by Christopher Rumpf on MM/DD/YY.
 //
 
 import Foundation
 
 class DayX: Day {
-  func part1() -> String {
-    "Not Implemented"
-  }
-  
-  func part2() -> String {
-    "Not Implemented"
-  }
+    func part1() -> String {
+        "Not Implemented"
+    }
+    
+    func part2() -> String {
+        "Not Implemented"
+    }
 }
 
