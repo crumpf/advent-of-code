538cb3b1687d117e145882a67694c283fd2267cc
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2022-12-06 00:53:27 -0500

    Day 7 Target Setup

---
+++ 2022/Swift/Advent of Code/Day 07/Day07.swift
@@ -0,0 +1,18 @@
+//
+//  Day07.swift
+//  Advent of Code
+//
+//  Created by Christopher Rumpf on 12/07/2022.
+//
+
+import Foundation
+
+class Day07: Day {
+    func part1() -> String {
+        return "Not Implemented"
+    }
+    
+    func part2() -> String {
+        return "Not Implemented"
+    }
+}
