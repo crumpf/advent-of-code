962801c8ee98cb715d8d4ee7b6d16d41a4e82fff
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2022-12-07 13:45:03 -0500

    Day 7

--- 2022/Swift/Advent of Code/Day 07/Day07.swift
+++ 2022/Swift/Advent of Code/Day 07/Day07.swift
@@ -12,24 +12,22 @@ class Day07: Day {
         let fs = filesystem(fromTerminalOutput: input)
         let result = fs.allDirs()
             .map { $0.totalSize() }
-            .filter { $0 <= 100000 }
+            .filter { $0 <= 100_000 }
             .reduce(0, +)
         return "\(result)"
     }
     
     func part2() -> String {
-        let totalSpace = 70000000
-        let requriedSpace = 30000000
+        let totalSpace = 70_000_000
+        let requriedSpace = 30_000_000
         let fs = filesystem(fromTerminalOutput: input)
         let usedSpace = fs.totalSize()
         let unusedSpace = totalSpace - usedSpace
         let neededSpace = requriedSpace - unusedSpace
-        
         let qualifyingDirs = fs.allDirs()
             .map { $0.totalSize() }
             .filter { $0 >= neededSpace }
             .sorted(by: <)
-
         return "\(qualifyingDirs.first ?? -1)"
     }
     
@@ -44,7 +42,7 @@ class Day07: Day {
                     let dirName = comps[1]
                     if dirName == "..", let parent = pwd.parent {
                         pwd = parent
-                    } else if let dir = pwd.subdirs[dirName] {
+                    } else if let dir = pwd.subdirs.first(where: { $0.name == dirName }) {
                         pwd = dir
                     }
                 }
@@ -56,10 +54,10 @@ class Day07: Day {
                         switch comps[0] {
                         case "dir":
                             let dir = Dir(parent: pwd, name: name)
-                            pwd.subdirs[dir.name] = dir
+                            pwd.subdirs.append(dir)
                         case let size where Int(size) != nil:
                             let file = File(dir: pwd, name: name, size: Int(size)!)
-                            pwd.files[file.name] = file
+                            pwd.files.append(file)
                         default:
                             break
                         }
@@ -75,8 +73,8 @@ class Day07: Day {
 class Dir {
     weak var parent: Dir?
     var name: String
-    var subdirs: [String:Dir] = [:]
-    var files: [String:File] = [:]
+    var subdirs: [Dir] = []
+    var files: [File] = []
     
     init(parent: Dir?, name: String) {
         self.parent = parent
@@ -84,15 +82,11 @@ class Dir {
     }
     
     func totalSize() -> Int {
-        files.values.map { $0.size }.reduce(0, +) + subdirs.values.reduce(0) { $0 + $1.totalSize() }
+        files.map { $0.size }.reduce(0, +) + subdirs.reduce(0) { $0 + $1.totalSize() }
     }
     
     func allDirs() -> [Dir] {
-        var dirs = [self]
-        for sub in subdirs.values {
-            dirs += sub.allDirs()
-        }
-        return dirs
+        subdirs.reduce(into: [self]) { $0 += $1.allDirs()}
     }
 }
 
