0d1c752db099330c83f9b922f38858460f3e5e02
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2022-12-07 22:48:12 -0500

    Day 8 prep

---
+++ 2022/Swift/Advent of Code/Day 08/Day08.swift
@@ -0,0 +1,18 @@
+//
+//  Day08.swift
+//  Advent of Code
+//
+//  Created by Christopher Rumpf on 12/07/2022.
+//
+
+import Foundation
+
+class Day08: Day {
+    func part1() -> String {
+        return "Not Implemented"
+    }
+    
+    func part2() -> String {
+        return "Not Implemented"
+    }
+}
