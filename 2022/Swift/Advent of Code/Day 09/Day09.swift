b323e345b21d11cd48fbe2096583743a28e597d7
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2022-12-08 22:34:34 -0500

    set up Day 09

---
+++ 2022/Swift/Advent of Code/Day 09/Day09.swift
@@ -0,0 +1,18 @@
+//
+//  Day09.swift
+//  Advent of Code
+//
+//  Created by Christopher Rumpf on 12/09/2022.
+//
+
+import Foundation
+
+class Day09: Day {
+    func part1() -> String {
+        return "Not Implemented"
+    }
+    
+    func part2() -> String {
+        return "Not Implemented"
+    }
+}
