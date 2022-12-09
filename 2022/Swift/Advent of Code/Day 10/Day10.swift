3d1ec2100aa5b92e37426fcd41106a8957ad42a8
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2022-12-09 10:38:15 -0500

    set up day 10

---
+++ 2022/Swift/Advent of Code/Day 10/Day10.swift
@@ -0,0 +1,18 @@
+//
+//  Day10.swift
+//  Advent of Code
+//
+//  Created by Christopher Rumpf on 12/10/22.
+//
+
+import Foundation
+
+class Day10: Day {
+    func part1() -> String {
+        "Not Implemented"
+    }
+    
+    func part2() -> String {
+        "Not Implemented"
+    }
+}
