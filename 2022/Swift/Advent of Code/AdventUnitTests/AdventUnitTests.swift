29ff7c111746fb8216d3853ac1fdaa16886dfb67
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2022-12-15 10:06:06 -0500

    Day 15

--- 2022/Swift/Advent of Code/AdventUnitTests/AdventUnitTests.swift
+++ 2022/Swift/Advent of Code/AdventUnitTests/AdventUnitTests.swift
@@ -413,8 +413,22 @@ final class AdventUnitTests: XCTestCase {
 
     func testDay15() throws {
         let day = Day15(input: """
+            Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+            Sensor at x=9, y=16: closest beacon is at x=10, y=16
+            Sensor at x=13, y=2: closest beacon is at x=15, y=3
+            Sensor at x=12, y=14: closest beacon is at x=10, y=16
+            Sensor at x=10, y=20: closest beacon is at x=10, y=16
+            Sensor at x=14, y=17: closest beacon is at x=10, y=16
+            Sensor at x=8, y=7: closest beacon is at x=2, y=10
+            Sensor at x=2, y=0: closest beacon is at x=2, y=10
+            Sensor at x=0, y=11: closest beacon is at x=2, y=10
+            Sensor at x=20, y=14: closest beacon is at x=25, y=17
+            Sensor at x=17, y=20: closest beacon is at x=21, y=22
+            Sensor at x=16, y=7: closest beacon is at x=15, y=3
+            Sensor at x=14, y=3: closest beacon is at x=15, y=3
+            Sensor at x=20, y=1: closest beacon is at x=15, y=3
             """)
-        XCTAssertEqual(day.part1(), "Not Implemented", "Part 1 Failed")
+        XCTAssertEqual(day.numberOfPositionsThatCannotContainABeacon(inRow: 10), 26, "Part 1 Failed")
         XCTAssertEqual(day.part2(), "Not Implemented", "Part 2 Failed")
     }
 
--- 2022/Swift/Advent of Code/Day 15/Day15.swift
+++ 2022/Swift/Advent of Code/Day 15/Day15.swift
@@ -6,13 +6,78 @@
 //
 
 import Foundation
+import RegexBuilder
 
 class Day15: Day {
     func part1() -> String {
-        "Not Implemented"
+        "\(numberOfPositionsThatCannotContainABeacon(inRow: 2_000_000))"
     }
     
     func part2() -> String {
         "Not Implemented"
+    }
+    
+    func numberOfPositionsThatCannotContainABeacon(inRow row: Int) -> Int {
+        let boundaries = findMapBoundaries()
+        var count = 0
+        for x in (boundaries.min.x - 500000)...(boundaries.max.x + 500000) {
+            let vector = SIMD2(x, row)
+            for (sensor, beacon) in closestBeacons {
+                if vector == beacon {
+                    continue
+                }
+                if vector.manhattanDistance(to: sensor) <= beacon.manhattanDistance(to: sensor) {
+                    count += 1
+                    break
+                }
+            }
+        }
+        return count
+    }
+    
+    // key=sensor position, value=closest beacon position
+    private lazy var closestBeacons: [SIMD2<Int>: SIMD2<Int>] = {
+        let numberCapture = Capture {
+            Optionally {
+                "-"
+            }
+            OneOrMore(.digit)
+        }
+        let regex = Regex {
+            "Sensor at x="
+            numberCapture
+            ", y="
+            numberCapture
+            ": closest beacon is at x="
+            numberCapture
+            ", y="
+            numberCapture
+        }
+        var closestBeacons: [SIMD2<Int>: SIMD2<Int>] = [:]
+        input.enumerateLines { line, stop in
+            for match in line.matches(of: regex).enumerated() {
+                let (_, sensorX, sensorY, beaconX, beaconY) = match.element.output
+                closestBeacons[SIMD2(Int(sensorX)!, Int(sensorY)!)] = SIMD2(Int(beaconX)!, Int(beaconY)!)
+            }
+        }
+        return closestBeacons
+    }()
+    
+    typealias Boundaries = (min: SIMD2<Int>, max: SIMD2<Int>)
+
+    private func findMapBoundaries() -> Boundaries {
+        closestBeacons.reduce(into: (min: SIMD2<Int>(Int.max, Int.max), max: SIMD2<Int>(Int.min, Int.min))) {
+            $0.min.x = min($0.min.x, min($1.key.x, $1.value.x))
+            $0.min.y = min($0.min.y, min($1.key.y, $1.value.y))
+            $0.max.x = max($0.max.x, max($1.key.x, $1.value.x))
+            $0.max.y = max($0.max.y, max($1.key.y, $1.value.y))
+        }
+    }
+}
+
+extension SIMD2 where Scalar == Int {
+    func manhattanDistance(to vector: Self) -> Scalar {
+        let delta = self &- vector
+        return abs(delta.x) + abs(delta.y)
     }
 }
