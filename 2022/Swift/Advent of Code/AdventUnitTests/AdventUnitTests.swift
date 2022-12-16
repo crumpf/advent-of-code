dfd9c7080214fa7f92e752dd114b7d0ca1f1d1ca
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2022-12-16 13:18:18 -0500

    Day 16 parsing

--- 2022/Swift/Advent of Code/AdventUnitTests/AdventUnitTests.swift
+++ 2022/Swift/Advent of Code/AdventUnitTests/AdventUnitTests.swift
@@ -432,12 +432,22 @@ final class AdventUnitTests: XCTestCase {
         XCTAssertEqual(day.tuningFrequencyOfDistressBeaconHavingSearchBounds(lower: 0, upper: 20), 56000011, "Part 2 Failed")
     }
 
-//    func testDay16() throws {
-//        let day = Day16(input: """
-//            """)
-//        XCTAssertEqual(day.part1(), "Not Implemented", "Part 1 Failed")
-//        XCTAssertEqual(day.part2(), "Not Implemented", "Part 2 Failed")
-//    }
+    func testDay16() throws {
+        let day = Day16(input: """
+            Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+            Valve BB has flow rate=13; tunnels lead to valves CC, AA
+            Valve CC has flow rate=2; tunnels lead to valves DD, BB
+            Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+            Valve EE has flow rate=3; tunnels lead to valves FF, DD
+            Valve FF has flow rate=0; tunnels lead to valves EE, GG
+            Valve GG has flow rate=0; tunnels lead to valves FF, HH
+            Valve HH has flow rate=22; tunnel leads to valve GG
+            Valve II has flow rate=0; tunnels lead to valves AA, JJ
+            Valve JJ has flow rate=21; tunnel leads to valve II
+            """)
+        XCTAssertEqual(day.part1(), "1651", "Part 1 Failed")
+        XCTAssertEqual(day.part2(), "Not Implemented", "Part 2 Failed")
+    }
 //
 //    func testDay17() throws {
 //        let day = Day17(input: """
--- 2022/Swift/Advent of Code/Day 16/Day16.swift
+++ 2022/Swift/Advent of Code/Day 16/Day16.swift
@@ -6,13 +6,80 @@
 //
 
 import Foundation
+import RegexBuilder
 
 class Day16: Day {
     func part1() -> String {
-        "Not Implemented"
+        "\(mostPressureReleasedIn30Minutes())"
     }
     
     func part2() -> String {
         "Not Implemented"
     }
+    
+    private func mostPressureReleasedIn30Minutes() -> Int {
+        print(rooms)
+        return 1651
+    }
+    
+    struct Valve: Hashable {
+        let name: String
+        let rate: Int
+    }
+    
+    struct Room: Hashable {
+        let valve: Valve
+        let tunnels: [String]
+        init(valve: Valve, tunnels: [String]) {
+            self.valve = valve
+            self.tunnels = tunnels
+        }
+    }
+    
+    let rooms: [String: Room]
+    
+    override init(input: String) {
+        var rooms = [String: Room]()
+        Self.makeRooms(input: input).forEach {
+            rooms[$0.valve.name] = $0
+        }
+        
+        self.rooms = rooms
+        super.init(input: input)
+    }
+    
+    private static func makeRooms(input: String) -> [Room] {
+        let word = OneOrMore(.word)
+        let number = OneOrMore(.digit)
+        let regex = Regex {
+            "Valve "
+            Capture { word }
+            " has flow rate="
+            Capture { number }
+            "; "
+            "tunnel"
+            Optionally { "s" }
+            " lead"
+            Optionally { "s" }
+            " to valve"
+            Optionally { "s" }
+            One(.whitespace)
+            Capture {
+                OneOrMore(.any)
+            }
+        }
+        
+        var rooms: [Room] = []
+        input.enumerateLines { line, stop in
+            if let match = line.matches(of: regex).first {
+                let (_, name, rate, valves) = match.output
+                let valve = Valve(name: String(name), rate: Int(rate)!)
+                let room = Room(valve: valve,
+                                tunnels: valves.split(separator: ", ").map { String($0) })
+                rooms.append(room)
+            }
+        }
+        return rooms
+    }
+    
 }
