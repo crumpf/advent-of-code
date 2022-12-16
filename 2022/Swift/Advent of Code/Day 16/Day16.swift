f30104325a1586b35a842cbd311995181cebcca2
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2022-12-16 11:42:12 -0500

    set up day 16

---
+++ 2022/Swift/Advent of Code/Day 16/Day16.swift
@@ -0,0 +1,18 @@
+//
+//  Day16.swift
+//  Advent of Code
+//
+//  Created by Christopher Rumpf on 12/16/22.
+//
+
+import Foundation
+
+class Day16: Day {
+    func part1() -> String {
+        "Not Implemented"
+    }
+    
+    func part2() -> String {
+        "Not Implemented"
+    }
+}
