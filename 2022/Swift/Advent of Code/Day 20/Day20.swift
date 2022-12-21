0aee26e36ff150234534d692dea844b0fa76ee55
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2022-12-20 19:21:43 -0500

    Update Day20.swift

--- 2022/Swift/Advent of Code/Day 20/Day20.swift
+++ 2022/Swift/Advent of Code/Day 20/Day20.swift
@@ -33,7 +33,9 @@ class Day20: Day {
                 case let offset where offset > 0:
                     return offset % list.count
                 case let offset where offset < 0:
-                    return list.count - (((list.count - index) + abs(coord.value)) % list.count)
+                    let reverseIndex = (((list.count - index) + abs(coord.value)) % list.count)
+                    if reverseIndex == 0 { return 0 }
+                    return list.count - reverseIndex
                 default:
                     return 0
                 }
@@ -49,11 +51,46 @@ class Day20: Day {
         return sum
     }
     
+    let decryptionKey = 811589153
+    
     func sumOfDecryptedGroveCoordinates() -> Int {
-        return 1623178306
+        
+        var list = CircularlyLinkedList<Coordinate>()
+        list.insert(decryptedList, at: 0)
+        
+        for _ in 1...10 {
+            for coord in decryptedList {
+                let index = list.firstIndex(of: coord)!
+                list.remove(at: index)
+                let newIndex = {
+                    switch index + coord.value {
+                    case let offset where offset > 0:
+                        return offset % list.count
+                    case let offset where offset < 0:
+                        let reverseIndex = (((list.count - index) + abs(coord.value)) % list.count)
+                        if reverseIndex == 0 { return 0 }
+                        return list.count - reverseIndex
+                    default:
+                        return 0
+                    }
+                }()
+                list.insert(coord, at: newIndex)
+            }
+        }
+        
+        var sum = 0
+        if let zeroIndex = list.firstIndex(where: { $0.value == 0} ) {
+            sum = [1000, 2000, 3000].map { list.element(at: (zeroIndex + $0) % list.count)!.value }.reduce(0, +)
+        }
+
+        return sum
     }
     
     private lazy var encryptedList = input.lines().enumerated().map { (n, value) in
         Coordinate(id: n, value: Int(value)!)
+    }
+    
+    private lazy var decryptedList = input.lines().enumerated().map { (n, value) in
+        Coordinate(id: n, value: Int(value)! * decryptionKey)
     }
 }
