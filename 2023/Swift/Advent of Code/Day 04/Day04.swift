70c95aba2b58a11de3ff16552bdf273f959ca7f1
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2023-12-04 00:56:47 -0500

    2023 day 4 part 1

--- 2023/Swift/Advent of Code/Day 04/Day04.swift
+++ 2023/Swift/Advent of Code/Day 04/Day04.swift
@@ -9,10 +9,43 @@ import Foundation
 
 class Day04: Day {
     func part1() -> String {
-        "Not Implemented"
+        "\(sumOfCardPoints())"
     }
     
     func part2() -> String {
         "Not Implemented"
+    }
+    
+    private func sumOfCardPoints() -> Int {
+        input.lines()
+            .map(makeCard(line:))
+            .map { card in
+                let myWinners = Set(card.winners).intersection(Set(card.have))
+                if myWinners.count > 0 {
+                    return 1 << (myWinners.count - 1)
+                }
+                return 0
+            }
+            .reduce(0, +)
+    }
+    
+    struct Card {
+        let id: Int
+        let winners: [Int]
+        let have: [Int]
+    }
+    
+    private func makeCard(line: String) -> Card {
+        // e.g. "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+        let comps = line.components(separatedBy: ":")
+        let id = Int(comps[0].dropFirst("Card".count).trimmingCharacters(in: .whitespaces))!
+        let nums = comps[1].components(separatedBy: "|").map {
+            $0.trimmingCharacters(in: .whitespaces)
+                .components(separatedBy: .whitespaces)
+                .compactMap {
+                    Int($0)
+                }
+        }
+        return Card(id: id, winners: nums[0], have: nums[1])
     }
 }
