606b4fcade5a6cec4f19ff80cf52398990714b5c
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2023-12-03 23:53:29 -0500

    2023 day 2 part 1

--- 2023/Swift/Advent of Code/AdventUnitTests/AdventUnitTests.swift
+++ 2023/Swift/Advent of Code/AdventUnitTests/AdventUnitTests.swift
@@ -40,8 +40,13 @@ final class AdventUnitTests: XCTestCase {
     
     func testDay02() throws {
         let day = Day02(input: """
+            Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
             """)
-        XCTAssertEqual(day.part1(), "Not Implemented", "Part 1 Failed")
+        XCTAssertEqual(day.part1(), "8", "Part 1 Failed")
         XCTAssertEqual(day.part2(), "Not Implemented", "Part 2 Failed")
     }
     
--- 2023/Swift/Advent of Code/Day 02/Day02.swift
+++ 2023/Swift/Advent of Code/Day 02/Day02.swift
@@ -1,18 +1,59 @@
 //
-//  DayX.swift
+//  Day02.swift
 //  Advent of Code
 //
-//  Created by Christopher Rumpf on MM/DD/YY.
+//  Created by Christopher Rumpf on 12/03/2023.
 //
+// --- Day 2: Cube Conundrum ---
 
 import Foundation
 
 class Day02: Day {
+    // Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
     func part1() -> String {
-        "Not Implemented"
+        "\(sumOfPossibleGameIds(withRed: 12, green: 13, blue: 14))"
     }
     
     func part2() -> String {
         "Not Implemented"
+    }
+    
+    typealias Reveal = (count: Int, color: String)
+    typealias Game = (id: Int, reveals: [[Reveal]])
+    
+    private func sumOfPossibleGameIds(withRed red: Int, green: Int, blue: Int) -> Int {
+        input.lines()
+            .map(makeGame(line:))
+            .filter { isGameValid(game: $0, red: red, green: green, blue: blue) }
+            .map { $0.id }
+            .reduce(0, +)
+    }
+    
+    private func makeGame(line: String) -> Game {
+        //Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        let comps = line.components(separatedBy: ":")
+        let id = Int(comps[0].dropFirst("Game ".count))!
+        let reveals = comps[1].components(separatedBy: ";").map {
+            $0.components(separatedBy: ",")
+            .map {
+                let split = $0.components(separatedBy: " ")
+                return Reveal(Int(split[1])!, split[2])
+            }
+        }
+        return Game(id, reveals)
+    }
+    
+    private func isGameValid(game: Game, red: Int, green: Int, blue: Int) -> Bool {
+        for reveal in game.reveals {
+            for cubes in reveal {
+                switch cubes {
+                case let (count, "red"): if count > red { return false }
+                case let (count, "green"): if count > green { return false }
+                case let (count, "blue"): if count > blue { return false }
+                default: break
+                }
+            }
+        }
+        return true
     }
 }
