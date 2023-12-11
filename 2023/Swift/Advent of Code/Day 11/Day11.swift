0c4ec787cc529a072332c3b736ad824bfab7ede8
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2023-12-11 10:13:52 -0500

    2023 day 11 part 1

--- 2023/Swift/Advent of Code/Day 11/Day11.swift
+++ 2023/Swift/Advent of Code/Day 11/Day11.swift
@@ -1,18 +1,62 @@
 //
-//  DayX.swift
+//  Day11.swift
 //  Advent of Code
 //
-//  Created by Christopher Rumpf on MM/DD/YY.
+//  Created by Christopher Rumpf on 12/11/23.
 //
+//  --- Day 11: Cosmic Expansion ---
 
 import Foundation
 
 class Day11: Day {
     func part1() -> String {
-        "Not Implemented"
+        let image = makeImage(input: input)
+        let expanded = expand(image: image)
+        let galaxies = galaxyLocations(in: expanded)
+        let sumOfShortestPathsBetweenAllGalaxies = galaxies.enumerated().map { gStart in
+            galaxies.dropFirst(gStart.offset + 1).reduce(0) { partialResult, gEnd in
+                let diff = gStart.element &- gEnd
+                return partialResult + abs(diff.x) + abs(diff.y)
+            }
+        }.reduce(0, +)
+        return "\(sumOfShortestPathsBetweenAllGalaxies)"
     }
     
     func part2() -> String {
         "Not Implemented"
+    }
+
+    typealias Image = [[Character]]
+    typealias Point = SIMD2<Int>
+
+    func makeImage(input: String) -> Image {
+        input.lines().map(Array.init)
+    }
+
+    func expand(image: Image) -> Image {
+        var expanded = Image()
+        for row in image {
+            expanded.append(row)
+            if !row.contains("#") { expanded.append(row) }
+        }
+        for x in image.first!.indices.reversed() {
+            let col = image.map { $0[x] }
+            if !col.contains("#") {
+                for y in expanded.indices {
+                    expanded[y].insert(".", at: x)
+                }
+            }
+        }
+        return expanded
+    }
+
+    func galaxyLocations(in image: Image) -> [Point] {
+        var points: [Point] = []
+        for (y, row) in image.enumerated() {
+            for (x, value) in row.enumerated() {
+                if value == "#" { points.append(Point(x, y)) }
+            }
+        }
+        return points
     }
 }
--- 2023/Swift/Advent of Code/Day 11/main.swift
+++ 2023/Swift/Advent of Code/Day 11/main.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 guard let fileInput = FileInput(pathRelativeToCurrentDirectory: "inputs/day/11/input") else { abort() }
+//guard let fileInput = FileInput(pathRelativeToCurrentDirectory: "inputs/day/11/example") else { abort() }
 var startTime = 0.0
 let day = Day11(input: fileInput.raw)
 
