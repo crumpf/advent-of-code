94e12b3a93429124e5b835c1406aa063f3f8c8ca
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2023-12-13 22:38:49 -0500

    2023 day 13 part 2

--- 2023/Swift/Advent of Code/Day 13/Day13.swift
+++ 2023/Swift/Advent of Code/Day 13/Day13.swift
@@ -16,25 +16,33 @@ class Day13: Day {
     }
     
     func part2() -> String {
-        "Not Implemented"
+        let patterns = makePatterns()
+        let summary = patterns.map { $0.summarizedWithSmudgeFixed() }.reduce(0, +)
+        return "\(summary)"
     }
 
     struct Pattern {
         let pattern: [[Character]]
+        let id: Int
 
-        init(string: String) {
+        init(string: String, id: Int) {
             pattern = string.lines().map(Array.init)
+            self.id = id
         }
 
         enum Direction {
             case vertical, horizontal
         }
 
-        func reflectionStart(direction: Direction) -> Int? {
+        func columns(of lines: [[Character]]) -> [[Character]] {
+            lines[0].indices.map { i in  lines.map { $0[i] }}
+        }
+
+        func reflectionIndex(direction: Direction) -> Int {
             let lines: [[Character]]
-                switch direction {
+            switch direction {
             case .vertical:
-                lines = pattern[0].indices.map { i in  pattern.map { $0[i] }}
+                lines = columns(of: pattern)
             case .horizontal:
                 lines = pattern
             }
@@ -46,15 +54,51 @@ class Day13: Day {
                     return i
                 }
             }
-            return nil
+            return 0
+        }
+
+        func reflectionIndex(direction: Direction, change: (x:Int, y:Int), differentThan: Int) -> Int {
+            var lines = pattern
+            lines[change.y][change.x] = lines[change.y][change.x] == "#" ? "." : "#"
+            if direction == .vertical {
+                lines = columns(of: lines)
+            }
+
+            for i in (1..<lines.indices.upperBound) {
+                let prev = lines[0..<i]
+                let next = lines.dropFirst(i)
+                if zip(prev.reversed(), next).allSatisfy({ $0 == $1 }) && i != differentThan {
+                    return i
+                }
+            }
+            return 0
         }
 
         func summarize() -> Int {
-            (reflectionStart(direction: .vertical) ?? 0) + (100 * (reflectionStart(direction: .horizontal) ?? 0))
+            summary(v: reflectionIndex(direction: .vertical), h: reflectionIndex(direction: .horizontal))
+        }
+
+        func summarizedWithSmudgeFixed() -> Int {
+            let smudgeV = reflectionIndex(direction: .vertical)
+            let smudgeH = reflectionIndex(direction: .horizontal)
+            for (y, line) in pattern.enumerated() {
+                for x in line.indices {
+                    let fixV = reflectionIndex(direction: .vertical, change: (x,y), differentThan: smudgeV)
+                    let fixH = reflectionIndex(direction: .horizontal, change: (x,y), differentThan: smudgeH)
+                    if fixV > 0 || fixH > 0 {
+                        return summary(v: fixV, h: fixH)
+                    }
+                }
+            }
+            abort() // !oh no, there's supposed to be a smudge in every pattern
+        }
+
+        private func summary(v: Int, h: Int) -> Int {
+            v + 100 * h
         }
     }
 
     private func makePatterns() -> [Pattern] {
-        return input.components(separatedBy: "\n\n").map(Pattern.init(string:))
+        return input.components(separatedBy: "\n\n").enumerated().map { Pattern.init(string:$0.element, id: $0.offset) }
     }
 }
