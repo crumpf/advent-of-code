20fe4c7c2559f8211c7b2428d535aa4999870dd9
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2023-12-15 09:26:38 -0500

    2023 day 15 part 1

--- 2023/Swift/Advent of Code/Day 15/Day15.swift
+++ 2023/Swift/Advent of Code/Day 15/Day15.swift
@@ -4,15 +4,36 @@
 //
 //  Created by Christopher Rumpf on MM/DD/YY.
 //
+//  --- Day 15: Lens Library ---
 
 import Foundation
 
 class Day15: Day {
     func part1() -> String {
-        "Not Implemented"
+        let seqs = makeSequences()
+        let sum = seqs.map(HASH(_:)).reduce(0, +)
+        return "\(sum)"
     }
     
     func part2() -> String {
         "Not Implemented"
+    }
+
+    private func makeSequences() -> [String] {
+        input.split(separator: ",").map { String($0) }
+    }
+
+    func HASH(_ s: String) -> Int {
+        var currentValue = 0
+        for c in s {
+            if c.isNewline { continue }
+            guard let ascii = c.asciiValue else {
+                abort() // unexpected non-ascii found
+            }
+            currentValue += Int(ascii)
+            currentValue *= 17
+            currentValue = currentValue % 256
+        }
+        return currentValue
     }
 }
