75368abf7f08b8a7b1f7097264b7c6f848e96b7f
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2024-12-03 12:48:06 -0500

    2024 day 01 part 1

--- 2024/swift-aoc-starter/Sources/Day01.swift
+++ 2024/swift-aoc-starter/Sources/Day01.swift
@@ -4,22 +4,25 @@ struct Day01: AdventDay {
   // Save your data in a corresponding text file in the `Data` directory.
   var data: String
 
-  // Splits input data into its component parts and convert from string.
-  var entities: [[Int]] {
-    data.split(separator: "\n\n").map {
-      $0.split(separator: "\n").compactMap { Int($0) }
+  // make lists from input data
+  func makeLists() -> ([Int], [Int]) {
+    data.split(separator: "\n").map {
+      $0.split(separator: " ").compactMap { Int($0) }
+    }.reduce(into: ([Int](), [Int]())) { partialResult, row in
+      partialResult.0.append(row[0])
+      partialResult.1.append(row[1])
     }
   }
 
   // Replace this with your solution for the first part of the day's challenge.
   func part1() -> Any {
-    // Calculate the sum of the first set of input data
-    entities.first?.reduce(0, +) ?? 0
+    let lists = makeLists()
+    // order the lists and add up distances between the entries
+    return zip(lists.0.sorted(), lists.1.sorted()).map { abs($0.0 - $0.1) }.reduce(0, +)
   }
 
   // Replace this with your solution for the second part of the day's challenge.
   func part2() -> Any {
-    // Sum the maximum entries in each set of data
-    entities.map { $0.max() ?? 0 }.reduce(0, +)
+    0
   }
 }
