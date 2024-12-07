69f70ad2f66c80855b883ecc5751bd885a1861d1
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2024-12-07 13:22:53 -0500

    2024 Day 7

--- 2024/swift-aoc-starter/Sources/Day07.swift
+++ 2024/swift-aoc-starter/Sources/Day07.swift
@@ -4,22 +4,38 @@ struct Day07: AdventDay {
   // Save your data in a corresponding text file in the `Data` directory.
   var data: String
 
-  // Splits input data into its component parts and convert from string.
-  var entities: [[Int]] {
-    data.split(separator: "\n\n").map {
-      $0.split(separator: "\n").compactMap { Int($0) }
+  func makeCalibrationEquations() -> [(Int, [Int])] {
+    data.split(separator: "\n").map {
+      let components = $0.split(separator: ": ")
+      return (Int(components[0])!, components[1].split(separator: " ").map { Int($0)! })
     }
   }
 
   // Replace this with your solution for the first part of the day's challenge.
   func part1() -> Any {
-    // Calculate the sum of the first set of input data
-    entities.first?.reduce(0, +) ?? 0
+    let equations = makeCalibrationEquations()
+    return equations.filter(isPossible(equation:)).reduce(0) { $0 + $1.0 }
   }
 
   // Replace this with your solution for the second part of the day's challenge.
   func part2() -> Any {
-    // Sum the maximum entries in each set of data
-    entities.map { $0.max() ?? 0 }.reduce(0, +)
+    0
+  }
+
+  func isPossible(equation: (Int, [Int])) -> Bool {
+    let testValue = equation.0
+    let numbers = equation.1
+    let operatorCount = equation.1.count - 1
+    for permutation in (0..<(1<<operatorCount)) {
+      let binary = String(permutation, radix: 2)
+      let padded = String(repeating: "0", count: operatorCount - binary.count) + binary
+      let result = zip(padded, numbers.dropFirst()).reduce(numbers.first!) { partialResult, elem in
+        elem.0 == "0" ? partialResult + elem.1 : partialResult * elem.1
+      }
+      if testValue == result {
+        return true
+      }
+    }
+    return false
   }
 }
