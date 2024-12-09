e9d593c3d29f2e103020cb7bcd0d665a222b8fad
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2024-12-09 10:21:51 -0500

    2024 day 9 part 1

--- 2024/swift-aoc-starter/Sources/Day09.swift
+++ 2024/swift-aoc-starter/Sources/Day09.swift
@@ -5,21 +5,48 @@ struct Day09: AdventDay {
   var data: String
 
   // Splits input data into its component parts and convert from string.
-  var entities: [[Int]] {
-    data.split(separator: "\n\n").map {
-      $0.split(separator: "\n").compactMap { Int($0) }
+  var diskMap: [Int] {
+    data.split(separator: "\n").first!.map { Int(String($0))! }
+  }
+
+  var checksum: Int {
+    var map = diskMap
+    var position = -1, checksum = 0, endIndex = map.endIndex - 1
+    endIndex -= endIndex % 2 == 0 ? 0 : 1 // make sure endIndex doesn't start on free space
+    for index in map.indices {
+      let isFreeSpace = index % 2 != 0
+      for _ in (0..<map[index]) {
+        position += 1
+        if !isFreeSpace {
+          let fileID = index / 2
+          checksum += position * fileID
+        } else {
+          guard endIndex > index else {
+            return checksum
+          }
+          while map[endIndex] == 0 {
+            endIndex -= 2
+            guard endIndex > index else {
+              return checksum
+            }
+          }
+          let fileID = endIndex / 2
+          checksum += position * fileID
+          map[endIndex] -= 1
+        }
+      }
     }
+    return checksum
   }
 
   // Replace this with your solution for the first part of the day's challenge.
   func part1() -> Any {
-    // Calculate the sum of the first set of input data
-    entities.first?.reduce(0, +) ?? 0
+    checksum
   }
 
   // Replace this with your solution for the second part of the day's challenge.
   func part2() -> Any {
     // Sum the maximum entries in each set of data
-    entities.map { $0.max() ?? 0 }.reduce(0, +)
+    0
   }
 }
--- 2024/swift-aoc-starter/Tests/Day09.swift
+++ 2024/swift-aoc-starter/Tests/Day09.swift
@@ -7,30 +7,17 @@ import Testing
 struct Day09Tests {
   // Smoke test data provided in the challenge question
   let testData = """
-    1000
-    2000
-    3000
-
-    4000
-
-    5000
-    6000
-
-    7000
-    8000
-    9000
-
-    10000
-
+    2333133121414131402
+    
     """
 
   @Test func testPart1() async throws {
     let challenge = Day09(data: testData)
-    #expect(String(describing: challenge.part1()) == "6000")
+    #expect(String(describing: challenge.part1()) == "1928")
   }
 
   @Test func testPart2() async throws {
     let challenge = Day09(data: testData)
-    #expect(String(describing: challenge.part2()) == "32000")
+    #expect(String(describing: challenge.part2()) == "0")
   }
 }
