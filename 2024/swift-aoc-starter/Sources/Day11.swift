d005a0267d251380551902145a32333a07632916
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2024-12-11 14:20:14 -0500

    Update Day11.swift

--- 2024/swift-aoc-starter/Sources/Day11.swift
+++ 2024/swift-aoc-starter/Sources/Day11.swift
@@ -5,29 +5,29 @@ struct Day11: AdventDay {
   var data: String
 
   // Splits input data into its component parts and convert from string.
-  var stones: [Int] {
-    data.split(separator: "\n").first!.split(separator: .whitespace).map { Int($0)! }
+  var stones: [Stone] {
+    data.split(separator: "\n").first!.split(separator: .whitespace).map { Stone(number: Int($0)!, blinks: 0) }
   }
 
   // Replace this with your solution for the first part of the day's challenge.
   func part1() -> Any {
-    var lookup = [ObservedStone: Int]()
+    var lookup = [Stone: Int]()
     return stones.map {
-      return countAfterObserving(stone: ObservedStone(number: $0, blinks: 0), times: 25, stoneCountLookup: &lookup)
+      countAfterObserving(stone: $0, times: 25, stoneCountLookup: &lookup)
     }
     .reduce(0, +)
   }
 
   // Replace this with your solution for the second part of the day's challenge.
   func part2() -> Any {
-    var lookup = [ObservedStone: Int]()
+    var lookup = [Stone: Int]()
     return stones.map {
-      return countAfterObserving(stone: ObservedStone(number: $0, blinks: 0), times: 75, stoneCountLookup: &lookup)
+      countAfterObserving(stone: $0, times: 75, stoneCountLookup: &lookup)
     }
     .reduce(0, +)
   }
 
-  func countAfterObserving(stone: ObservedStone, times: Int, stoneCountLookup: inout [ObservedStone: Int]) -> Int {
+  func countAfterObserving(stone: Stone, times: Int, stoneCountLookup: inout [Stone: Int]) -> Int {
     guard stone.blinks < times else { return 1 }
 
     if let count = stoneCountLookup[stone] {
@@ -42,21 +42,21 @@ struct Day11: AdventDay {
     .reduce(0, +)
   }
 
-  struct ObservedStone: Hashable {
+  struct Stone: Hashable {
     let number: Int
     let blinks: Int
 
-    func blink() -> [ObservedStone] {
+    func blink() -> [Stone] {
       if number == 0 {
-        return [ObservedStone(number: 1, blinks: blinks + 1)]
+        return [Stone(number: 1, blinks: blinks + 1)]
       }
       let stoneString = "\(number)"
       if stoneString.count.isMultiple(of: 2) {
         let half = stoneString.count / 2
-        return [ObservedStone(number: Int(stoneString.dropLast(half))!, blinks: blinks + 1),
-                ObservedStone(number: Int(stoneString.dropFirst(half))!, blinks: blinks + 1)]
+        return [Stone(number: Int(stoneString.dropLast(half))!, blinks: blinks + 1),
+                Stone(number: Int(stoneString.dropFirst(half))!, blinks: blinks + 1)]
       }
-      return [ObservedStone(number: number * 2024, blinks: blinks + 1)]
+      return [Stone(number: number * 2024, blinks: blinks + 1)]
     }
   }
 }
