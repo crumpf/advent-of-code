60567e23376b215c911d3b8e94d6cae0c572f217
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2024-12-13 08:55:14 -0500

    unga bunga solution for day 13 part 1

--- 2024/swift-aoc-starter/Sources/Day13.swift
+++ 2024/swift-aoc-starter/Sources/Day13.swift
@@ -6,21 +6,17 @@ struct Day13: AdventDay {
 
   func makeClawMachines() -> [ClawMachine] {
     data.split(separator: "\n\n").map {
-      let behavior = $0.split(separator: "\n")
-      let buttonARegex = /Button A: X\+(\d+), Y\+(\d+)/
-      let buttonBRegex = /Button B: X\+(\d+), Y\+(\d+)/
-      let prizeRegex = /Prize: X=(\d+), Y=(\d+)/
-      let (_, ax, ay) = $0.matches(of: buttonARegex)[0].output
-      let (_, bx, by) = $0.matches(of: buttonBRegex)[0].output
-      let (_, px, py) = $0.matches(of: prizeRegex)[0].output
+      let (_, ax, ay) = $0.matches(of: /Button A: X\+(\d+), Y\+(\d+)/)[0].output
+      let (_, bx, by) = $0.matches(of: /Button B: X\+(\d+), Y\+(\d+)/)[0].output
+      let (_, px, py) = $0.matches(of: /Prize: X=(\d+), Y=(\d+)/)[0].output
       return ClawMachine(a: (Int(ax)!,Int(ay)!), b: (Int(bx)!,Int(by)!), prize: (Int(px)!,Int(py)!))
     }
   }
   
   // Replace this with your solution for the first part of the day's challenge.
   func part1() -> Any {
-    let cm = makeClawMachines()
-    return cm.count
+    let machines = makeClawMachines()
+    return machines.compactMap { $0.minimumCost }.reduce(0) { $0 + $1.tokens }
   }
 
   // Replace this with your solution for the second part of the day's challenge.
@@ -32,5 +28,22 @@ struct Day13: AdventDay {
     let a: (x: Int, y: Int)
     let b: (x: Int, y: Int)
     let prize: (x: Int, y: Int)
+
+    var minimumCost: (tokens: Int, aPresses: Int, bPresses: Int)? {
+      var minCost: (tokens: Int, aPresses: Int, bPresses: Int)? = nil
+      for pressA in (1...100) {
+        for pressB in (1...100) {
+          let x = pressA * a.x + pressB * b.x
+          let y = pressA * a.y + pressB * b.y
+          if x == prize.x && y == prize.y {
+            let cost = pressA * 3 + pressB
+            if minCost == nil || cost < minCost!.tokens {
+              minCost = (cost, pressA, pressB)
+            }
+          }
+        }
+      }
+      return minCost
+    }
   }
 }
