2f1bbdb6cfc45eb54562a8066cc8c51f03dd0bbb
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2025-12-08 17:37:47 -0500

    2025 day 2 part 1

---
+++ 2025/swift-aoc-starter/Sources/Day02.swift
@@ -0,0 +1,51 @@
+import Algorithms
+
+struct Day02: AdventDay {
+  // Save your data in a corresponding text file in the `Data` directory.
+  var data: String
+
+  struct Range {
+    let min, max: String
+  }
+  
+  var ranges: [Range]{
+    data.trimmingCharacters(in: .newlines).split(separator: ",").map {
+      let r = $0.split(separator: "-")
+      return Range(min: String(r[0]), max: String(r[1]))
+    }
+  }
+
+  // Replace this with your solution for the first part of the day's challenge.
+  func part1() -> Any {
+    sumOfInvalidIds(in: ranges)
+  }
+
+  // Replace this with your solution for the second part of the day's challenge.
+  func part2() -> Any {
+    0
+  }
+  
+  func sumOfInvalidIds(in ranges: [Range]) -> Int {
+    var invalidSum = 0
+    
+    ranges.forEach { r in
+      let min = Int(r.min)!
+      let max = Int(r.max)!
+      
+      (min...max).forEach { i in
+        let str = "\(i)"
+        let len = str.count
+        if len.isMultiple(of: 2) {
+          let first = str.dropLast(len / 2)
+          let last = str.dropFirst(len / 2)
+          if first == last {
+            invalidSum += i
+          }
+        }
+      }
+    }
+    
+    return invalidSum
+  }
+  
+}
