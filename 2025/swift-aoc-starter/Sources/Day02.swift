fe5f82d42813061c3d94c07223450f607b776076
Chris Rumpf <228142+crumpf@users.noreply.github.com>
2025-12-08 18:12:11 -0500

    2025 day 2 part 2

--- 2025/swift-aoc-starter/Sources/Day02.swift
+++ 2025/swift-aoc-starter/Sources/Day02.swift
@@ -1,4 +1,5 @@
 import Algorithms
+import RegexBuilder
 
 struct Day02: AdventDay {
   // Save your data in a corresponding text file in the `Data` directory.
@@ -22,7 +23,7 @@ struct Day02: AdventDay {
 
   // Replace this with your solution for the second part of the day's challenge.
   func part2() -> Any {
-    0
+    sumOfAllSillyIds(in: ranges)
   }
   
   func sumOfInvalidIds(in ranges: [Range]) -> Int {
@@ -48,4 +49,26 @@ struct Day02: AdventDay {
     return invalidSum
   }
   
+  func sumOfAllSillyIds(in ranges: [Range]) -> Int {
+    var invalidSum = 0
+    
+    ranges.forEach { r in
+      let min = Int(r.min)!
+      let max = Int(r.max)!
+      
+      (min...max).forEach { i in
+        let str = "\(i)"
+        // regex explainer:
+        //  •  (.+) captures any non-empty substring.
+        //  •  \1+ means “one or more repetitions of whatever was captured.”
+        let regex = /^(.+)\1+$/
+        if let _  = str.firstMatch(of: regex) {
+          invalidSum += i
+        }
+      }
+    }
+    
+    return invalidSum
+  }
+
 }
