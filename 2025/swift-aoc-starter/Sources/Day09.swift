4d60d964d5b3ad3d677eebc30019c47a2aaca433
Chris Rumpf <chris.rumpf@genesys.com>
2025-12-09 21:35:01 -0500

    2025 day 9 part 2

--- 2025/swift-aoc-starter/Sources/Day09.swift
+++ 2025/swift-aoc-starter/Sources/Day09.swift
@@ -7,6 +7,32 @@ struct Day09: AdventDay {
   struct Point : Hashable {
     var x: Int
     var y: Int
+
+    func area(to: Point) -> Int {
+      (abs(to.x - x) + 1) * (abs(to.y - y) + 1)
+    }
+  }
+
+  struct Rect : Hashable {
+    let p1: Point
+    let p2: Point
+    let area: Int
+
+    init(p1: Point, p2: Point) {
+      self.p1 = p1
+      self.p2 = p2
+      self.area = p1.area(to: p2)
+    }
+
+    func doesPerimiterCrossSegment(from a: Point, to b: Point) -> Bool {
+      let segX = min(a.x, b.x) ... max(a.x, b.x)
+      let segY = min(a.y, b.y) ... max(a.y, b.y)
+      // shave of area because the area has to fully cross the edge segments
+      let horz = (min(p1.x, p2.x) + 1) ... (max(p1.x, p2.x) - 1)
+      let vert = (min(p1.y, p2.y) + 1) ... (max(p1.y, p2.y) - 1)
+
+      return segX.overlaps(horz) && segY.overlaps(vert)
+    }
   }
 
   var redTilePoints: [Point] {
@@ -19,10 +45,10 @@ struct Day09: AdventDay {
   func part1() -> Any {
     let redTiles = redTilePoints
     var largestArea: Int = 0
-    
+
     for i in redTiles.indices {
       for j in i+1..<redTiles.endIndex {
-        let area = area(from: redTiles[i], to: redTiles[j])
+        let area = redTiles[i].area(to: redTiles[j])
         if area > largestArea {
           largestArea = area
         }
@@ -33,10 +59,28 @@ struct Day09: AdventDay {
   }
 
   func part2() -> Any {
-    0
+    let redTiles = redTilePoints
+    let sortedRects = redTiles.enumerated().flatMap { i, tile in
+      redTiles[i+1..<redTiles.endIndex].map { otherTile in
+        Rect(p1: tile, p2: otherTile)
+      }
+    }
+      .sorted { a, b in
+        a.area > b.area
+      }
+    let perimeterSegments = Array(zip(redTiles, redTiles.dropFirst())) + [(redTiles.last!, redTiles.first!)]
+
+    for rect in sortedRects {
+      // check to see if the edges of the rect cross any line segments from our red tile data which are the perimeter coordinates
+      let noOverlaps = perimeterSegments.allSatisfy { (a, b) in
+        !rect.doesPerimiterCrossSegment(from: a, to: b)
+      }
+      if noOverlaps {
+        return rect.area
+      }
+    }
+
+    return sortedRects[0].area
   }
 
-  private func area(from: Point, to: Point) -> Int {
-    (abs(to.x - from.x) + 1) * (abs(to.y - from.y) + 1)
-  }
 }
